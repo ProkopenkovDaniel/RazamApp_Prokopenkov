d72a302bcc26a655181cddb34d78ebcb1a256df3
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-09-29 00:38:03 +0300

    Made Controller

--- RazamApp/RazamApp/Controllers/HomeController.cs
+++ RazamApp/RazamApp/Controllers/HomeController.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RazamApp.Models;
 
 namespace RazamApp.Controllers
 {
     public class HomeController : Controller
     {
+        MessageContext db = new MessageContext();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            IEnumerable<Message> messages = db.Messages;
+            ViewBag.Messages = messages;
             return View();
         }
     }
