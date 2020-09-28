d8ab6c06c1585a154f99d6b8550505255374a45a
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-09-29 01:43:52 +0300

    Made view and added date

---
+++ RazamApp/RazamApp/Models/MessageInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Design;
+namespace RazamApp.Models
+{
+    public class MessageInitializer : DropCreateDatabaseAlways<MessageContext>
+    {
+        protected override void Seed(MessageContext db)
+        {
+
+        }
+    }
+}
