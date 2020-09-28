ca7f4f3cef9f443c1b6d4681a97f0cff533a6fd3
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-09-29 00:15:19 +0300

    Made Model for 'Hello'-app

---
+++ RazamApp/RazamApp/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazamApp.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string content { get; set; }
+    }
+}
---
+++ RazamApp/RazamApp/Models/MessageContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazamApp.Models
+{
+    public class MessageContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
