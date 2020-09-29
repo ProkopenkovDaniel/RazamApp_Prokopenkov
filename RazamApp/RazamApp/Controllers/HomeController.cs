a690a0930ece9f98634d8f3b62506fa516ca7d84
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-09-29 17:21:44 +0300

    Corrections 29.09.2020

--- RazamApp/RazamApp/Controllers/HomeController.cs
+++ RazamApp/RazamApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RazamApp.Models;
+using RazamApp.Util;
 
 namespace RazamApp.Controllers
 {
@@ -12,9 +13,34 @@ namespace RazamApp.Controllers
         MessageContext db = new MessageContext();
         public ActionResult Index()
         {
-            IEnumerable<Message> messages = db.Messages;
-            ViewBag.Messages = messages;
-            return View();
+           IEnumerable<Message> messages = db.Messages;
+           ViewBag.Messages = messages;
+            /* так, здесь возвращается View, который задан файлос Index.cshtml, 
+             * то есть одноимённый с методов. То есть здесь происходит рендеринг
+             * некоторой страницы и показывает её пользователю. Это может быть н обязательно
+             * та же самая страница, может быть и другая, но суть в том, что возвращается 
+             * некоторое представление*/
+            return View(db.Messages);
         }
+
+        public ActionResult GetHtml()
+        {
+
+            //использование класса результата дейтсвий
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public ActionResult GetContext()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return Content("<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>");
+        }
+
+        
     }
 }
